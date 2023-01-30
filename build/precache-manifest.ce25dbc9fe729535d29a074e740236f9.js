e5316ef8e44f52892a366f238c31800a24fa9a30
Jamal Khan <ahtishamkayani01@gmail.com>
2023-01-30 21:49:15 +0500

    first commit

---
+++ build/precache-manifest.ce25dbc9fe729535d29a074e740236f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1711a48b107a9d497c0e",
+    "url": "/static/css/main.97e1a5f0.chunk.css"
+  },
+  {
+    "revision": "1711a48b107a9d497c0e",
+    "url": "/static/js/main.1711a48b.chunk.js"
+  },
+  {
+    "revision": "40bf726894a3b7c60e21",
+    "url": "/static/js/1.40bf7268.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "10fe5a6e94639f604506891510730d02",
+    "url": "/index.html"
+  }
+];
